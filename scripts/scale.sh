496a0c96ee0941685a53346c0fadff285b54d276
Donald Rauscher <donald.rauscher@gmail.com>
2018-03-19 20:52:10 -0500

    Flip order (kubectl scale --> terraform apply)

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -1,5 +1,5 @@
-export PROJECT_ID=$(gcloud config get-value project -q)
-terraform apply -var project=${PROJECT_ID} -var node_count=$1 -auto-approve
-
 export WORKER_DEPLOY=$(kubectl get deploy --selector=component=worker --output=name)
 kubectl scale --replicas=$2 ${WORKER_DEPLOY}
+
+export PROJECT_ID=$(gcloud config get-value project -q)
+terraform apply -var project=${PROJECT_ID} -var node_count=$1 -auto-approve
